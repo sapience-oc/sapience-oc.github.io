8aa8f3f57621da088bcc85992b84c3b67f22a930
Versianih <versianienzo@gmail.com>
2026-07-16 11:00:59 -0300

    .

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://srv1826188.hstgr.cloud';
 
 export function getImageUrl(imagePath) {
   if (!imagePath) return null;
